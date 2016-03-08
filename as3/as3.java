dec4c1f6ec794a5dda077ebb0f6a7f61dfd9f306
Ethan Hamman <e.hamman@telus.net>
2016-03-08 10:42:46 -0700

    Basics

---
+++ as3/as3.java
@@ -0,0 +1,53 @@
+/*
+   Author: Ethan Hamman
+   StudentID: 10125341
+   Main class: reads files, inserts into Trie, and checks spelling
+*/
+import java.io.*;
+
+public class as3{
+
+   public static void main(String [] args){
+
+      if(args.length != 2){
+         System.out.println("Usage: 'as3 dictionary file_to_check'.");
+         System.exit(0);
+      }
+
+      String dictionary = args[0];
+      String spellCheck = args[1];
+
+      try{
+         FileReader fr = new FileReader(dictionary);
+         BufferedReader br = new BufferedReader(fr);
+
+         String line = br.readLine();
+         while(line != null){
+            //Insert dictionary items
+
+            line = br.readLine();
+         }
+
+         br.close();
+         fr.close();
+
+         FileReader frs = new FileReader(spellCheck);
+         BufferedReader brs = new BufferedReader(fr);
+
+         String line = brs.readLine();
+         while(line != null){
+            //Spell Check all items
+
+            line = brs.readLine();
+         }
+
+
+         brs.close();
+         frs.close();
+
+      }catch(IOException e){
+         System.out.println("IOException: "+e);
+         System.exit(0);
+      }
+   }
+}
---
+++ as3/trieNode.java
@@ -0,0 +1,21 @@
+/*
+   Author: Ethan Hamman
+   StudentID: 10125341
+   Structure for the Trie
+   I am following a tree I found on Wikipedia, it goes as follows:
+   Each node has siblings and children, the siblings represent letters that can substitute each other, and children represent letters that
+   come after the parent
+   https://en.wikipedia.org/wiki/Trie#/media/File:Pointer_implementation_of_a_trie.svg
+*/
+
+public class trieNode{
+
+   public void trieNode{
+
+
+   }
+
+   public void insert{}
+   public String search{}
+
+}
